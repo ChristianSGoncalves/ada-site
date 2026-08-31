3886c7a0c6323f20fda1b5120aabdf8a29bc3cfb
ChristianSGoncalves <christian-goncalves@hotmail.com>
2026-08-31 03:15:02 +0000

    .

--- es/config.js
+++ es/config.js
@@ -4,15 +4,18 @@
 const APP_CONFIG = {
     production: {
         domain: 'adaservicos.com.br',
-        recaptchaKey: '6LeKi6AtAAAAAGjoWF_Se7ebyCrif9auSVba8mOP'
+        recaptchaKey: '6LeKi6AtAAAAAGjoWF_Se7ebyCrif9auSVba8mOP',
+        formEndpoint: 'https://formspree.io/f/contato@adaservicos.com.br'
     },
     homologation: {
         domain: 'hml.adaservicos.com.br',
-        recaptchaKey: '6Le_CKEtAAAAAEcQ5ZmVbj7w8qXZW5Xy5nIY45si'
+        recaptchaKey: '6Le_CKEtAAAAAEcQ5ZmVbj7w8qXZW5Xy5nIY45si',
+        formEndpoint: 'https://formspree.io/f/contato@adaservicos.com.br'
     },
     development: {
         domain: 'christiansgoncalves.github.io',
-        recaptchaKey: '6LePCaEtAAAAAGJ7hmlxosxyWI7wNhdGrum5HD7Q'
+        recaptchaKey: '6LePCaEtAAAAAGJ7hmlxosxyWI7wNhdGrum5HD7Q',
+        formEndpoint: 'https://formspree.io/f/contato@adaservicos.com.br' // Recomendo trocar pelo ID do Formspree aqui
     }
 };
 
@@ -21,11 +24,14 @@ function getEnvironmentConfig() {
     
     // Suporta o domínio raiz e o prefixo www para produção
     if (hostname === APP_CONFIG.production.domain || hostname === `www.${APP_CONFIG.production.domain}`) {
+        console.log("Ambiente: Produção");
         return APP_CONFIG.production;
     } else if (hostname === APP_CONFIG.homologation.domain || hostname.includes('hml.')) {
+        console.log("Ambiente: Homologação");
         return APP_CONFIG.homologation;
     } else {
-        // Padrão para desenvolvimento (localhost, dev.adaservicos.com.br ou URLs do github.io)
+        // Padrão para desenvolvimento
+        console.log("Ambiente: Desenvolvimento (GitHub/Local)");
         return APP_CONFIG.development;
     }
 }
